e26026c802e171ce4d4fee5ea821e08ed4e2f654
subitgit <suzaku_bitex@163.com>
2016-08-16 19:15:40 +0800

    complie

---
+++ app/src/main/java/com/bwf/tuanche/ui/carselect/CarSelectMainActivity.java
@@ -0,0 +1,36 @@
+package com.bwf.tuanche.ui.carselect;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.bwf.framwork.base.BaseActivity;
+import com.bwf.tuanche.R;
+
+public class CarSelectMainActivity extends BaseActivity {
+
+    @Override
+    public int getContentViewId() {
+        return R.layout.activity_car_select_main;
+    }
+
+    @Override
+    public void beforeInitView() {
+
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
---
+++ app/src/main/java/com/bwf/tuanche/ui/carselect/fragment/BrandCarFragment.java
@@ -0,0 +1,43 @@
+package com.bwf.tuanche.ui.carselect.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bwf.framwork.base.BaseFragment;
+import com.bwf.tuanche.R;
+
+/**
+ * 品牌选车
+ */
+public class BrandCarFragment extends BaseFragment {
+
+
+    @Override
+    protected int getResource() {
+        return R.layout.fragment_brand_car;
+    }
+
+    @Override
+    protected void beforeInitView() {
+
+    }
+
+    @Override
+    protected void initView(View rootView) {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
