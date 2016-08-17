0a7f54a8ce877eeb04f60649d930f46d9c93d23d
sandalli <707417775@qq.com>
2016-08-17 10:51:05 +0800

    201681701

--- app/src/main/java/com/bwf/tuanche/MainActivity.java
+++ app/src/main/java/com/bwf/tuanche/MainActivity.java
@@ -1,13 +1,41 @@
 package com.bwf.tuanche;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.bwf.framwork.base.BaseActivity;
+import com.bwf.framwork.utils.IntentUtils;
+import com.bwf.tuanche.ui.detail.CarDetailActivity;
+
+public class MainActivity extends BaseActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public int getContentViewId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    public void beforeInitView() {
+
+    }
+
+    @Override
+    public void initView() {
+        findViewByIdNoCast(R.id.text).setOnClickListener(this);
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.text) {
+            IntentUtils.openActivity(this, CarDetailActivity.class);
+        }
     }
 }
---
+++ app/src/main/java/com/bwf/tuanche/ui/detail/CarDetailActivity.java
@@ -0,0 +1,15 @@
+package com.bwf.tuanche.ui.detail;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.bwf.tuanche.R;
+
+public class CarDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_car_detail);
+    }
+}
